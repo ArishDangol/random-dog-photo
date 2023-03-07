58ef183e797c60c81f63b6a20bb48da652a194fa
arish dangol <arish.dangol@gmail.com>
2023-03-07 22:55:49 +0545

    project completed

---
+++ dog.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import webbrowser
+
+def doggie():
+    f = r"https://random.dog/woof.json"
+    page = requests.get(f)
+    data = json.loads(page.text)
+    webbrowser.open(data["url"])
+doggie()
